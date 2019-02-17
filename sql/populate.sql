4433a86843d8e465436da0bc178d2a1f3ea81b0f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-02-17 08:32:42 -0300

    create a initial populate data

---
+++ sql/populate.sql
@@ -0,0 +1,149 @@
+INSERT INTO public.categories (id, name, title, subtitle, thumb) VALUES (1, 'Games', 'Games', 'Games', 'http://static.jogos360.com.br/files/imagens/sonic1.jpg');
+INSERT INTO public.categories (id, name, title, subtitle, thumb) VALUES (2, 'Livros', 'Livros', 'Livros', 'https://cursodelinguagemcorporal.com/wp-content/uploads/2017/03/livros-de-linguagem-corporal.png');
+
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Livro - Vade Mecum Tradicional 2019', 2, 'Pioneira na exemplar técnica desenvolvida de atualização de Códigos e Legislação, como comprova o avançado número de suas edições e versões, a Editora Saraiva apresenta a edição aumentada e atualizada de sua principal obra: o Vade Mecum Saraiva.
+
+Com novo projeto gráfico, conta com o acréscimo de mais de 140 diplomas (entre leis, decretos, regimentos internos etc.) em relação à edição anterior. Assim, passa a reunir as normas mais importantes do ordenamento jurídico brasileiro, com destaque para:
+
+- Código Penal Militar, Código de Processo Penal Militar e Estatuto dos Militares
+
+- Legislação Previdenciária
+
+- Principais Convenções Internacionais
+
+- Regimentos Internos do STF e do STJ (divisão exclusiva = agilidade na pesquisa)
+
+O conteúdo foi criteriosamente selecionado com base na grade curricular das principais faculdades de direito e em editais de concursos para carreiras jurídicas.
+
+Sua estrutura foi pensada de forma a proporcionar o maior número de diplomas legislativos sem comprometer a legibilidade e o trabalho editorial.
+
+As notas de correlação entre as matérias, já tão tradicionais nas obras de legislação Saraiva, foram mantidas, assim como os índices multifuncionais, que permitem rápida localização de qualquer assunto por ordem alfabética, cronológica, pelo número do diploma ou por seu tema.
+
+Tradição, organização e estrutura, aliadas à confiabilidade do nome Saraiva, explicam que seja há mais de 10 anos o produto de legislação mais vendido no mercado.
+
+Últimas Atualizações da 27ª edição (fechamento em 4-1-2019):
+
+- Código Civil
+
+Condomínio em multipropriedade - Lei n. 13.777, de 20-12-2018
+
+Quórum de deliberação nas sociedades limitadas - Lei n. 13.792, de 3-1-2019
+
+- Código de Processo Civil
+
+Processos eletrônicos - Lei n. 13.793, de 3-1-2019
+
+- Código Penal
+
+Importunação sexual, estupros coletivo e corretivo e divulgação de sexo, estupro ou nudez de vulnerável ¿ Lei n. 13.718, de 24-9-2018
+
+Exposição da intimidade sexual ¿ Lei n. 13.772, de 19-12-2018
+
+- Código de Processo Penal
+
+Prisão domiciliar para gestante ¿ Lei n. 13.769, de 19-12-2018
+
+- CLT
+
+Falta justificada ao trabalho para realização de exames preventivos de câncer ¿ Lei n. 13.767, de 18-12-2018
+
+- Estatuto da Criança e do Adolescente
+
+Prevenção de gravidez na adolescência ¿ Lei n. 13.798, de 3-1-2019
+
+- Estatuto da OAB
+
+Honorários assistenciais - Lei n. 13.725, de 4-10-2018
+
+Processos eletrônicos ¿ Lei n. 13.793, de 3-1-2019
+
+- Reforma Trabalhista ¿ aplicação de normas processuais da CLT (IN n. 41, de 21-6-2018, do TST)
+
+- Lei de Proteção de Dados Pessoais (Lei n. 13.709, de 14-8-2018)
+
+- Trabalho temporário no Mercosul (Decreto n. 9.499, de 10-9-2018)
+
+- Execução indireta de serviços na Administração Pública (Decreto n. 9.507, de 21-9-2018)
+
+- Tratado de Marraqueche (Decreto n. 9.522, de 8-10-2018)
+
+- Juizados Especiais - contagem de prazos processuais (Lei n. 13.728, de 31-10-2018)
+
+- Gestão coletiva de direitos autorais (Decreto n. 9.574, de 22-11-2018)
+
+- Criança, adolescente e aprendiz (Decreto n. 9.579, de 22-11-2018)
+
+- Organização da Justiça Militar ¿ alterações (Lei n. 13.774, de 19-12-2018)
+
+- Duplicata eletrônica (Lei n. 13.775, de 20-12-2018)
+
+- Contratos de compra e venda ¿ resolução por inadimplência (Lei n. 13.786, de 27-12-2018)
+
+- Autoridade Nacional de Proteção de Dados Pessoais (MP 869, de 27-12-2018)
+
+- Ausência escolar por motivo religioso (Lei n. 13.796, de 3-1-2019)
+
+- Facilitação da posse de arma (alterações no Decreto do Desarmamento)', 2, 'Imagens meramente ilustrativas.
+Todas as informações divulgadas são de responsabilidade do Fabricante/Fornecedor.', 163.53, 'https://images-submarino.b2w.io/produtos/01/00/offers/01/00/item/134178/7/134178728_1GG.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Livro - Um Estranho Irresistível', 3, 'Uma mulher que desafia seu tempo. Dr. Garret Gibson, a única médica mulher na Inglaterra, é tão ousada e independente quanto qualquer homem ¿ por que não lidar com os próprios desejos como se fosse um? No entanto, ela nunca ficou tentada a se envolver com alguém, até agora. Ethan Ransom, um ex-detetive da Scotland Yard, é tão galante quanto secreto, e sua lealdade é um verdadeiro mistério. Em uma noite emocionante, eles cedem a uma poderosa atração mútua antes de se tornarem estranhos novamente.
+
+Um homem que quebra todas as regras. Ethan tem pouco interesse pela alta sociedade, mas é cativado pela preciosa e bela Garrett. Apesar da promessa de resistir um ao outro depois daquela noite sublime, ela logo será atraída para sua tarefa mais perigosa. Quando a missão dá errado, Garret usa toda a sua habilidade e coragem para se salvar. À medida que enfrentam a ameaça de uma traição do governo, Ethan fica disposto a assumir qualquer risco pelo amor da mulher mais extraordinária que já conheceu.
+
+"Kleypas continua a conquistar os corações dos leitores nesse livro dos Ravenels. Não há nada como um romance de Lisa Kleypas." ¿ RT Book Reviews
+
+"Uma história de amor para saborear, com prosa elegante, uma fascinante heroína e uma Londres vitoriana construída com detalhes históricos convincentes." ¿ Kirkus Reviews', 2, 'Imagens meramente ilustrativas.', 34.99, 'https://images-submarino.b2w.io/produtos/01/00/offers/01/00/item/134171/1/134171147SZ.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Livro - Dois a Dois', 1, 'Traduzidos para 50 idiomas, os livros de Nicholas Sparks já venderam mais de 100 milhões de exemplares no mundo.
+
+"Uma das melhores obras da carreira de Nicholas Sparks." - StarNews Online
+
+"Dois a dois faz o leitor rir, chorar e refletir. Uma história comovente que fala não apenas do amor de um pai e uma filha, mas também do poder da família e da coragem que devemos buscar em nós mesmos quando enfrentamos o desconhecido." - Booklist
+
+Com uma carreira bem-sucedida, uma linda esposa e uma adorável filha de 6 anos, Russell Green tem uma vida de dar inveja. Ele está tão certo de que essa paz reinará para sempre que não percebe quando a situação começa a sair dos trilhos.
+
+Em questão de meses, Russ perde o emprego e a confiança da esposa, que se afasta dele e se vê obrigada a voltar a trabalhar. Precisando lutar para se adaptar a uma nova realidade, ele se desdobra para cuidar da filhinha, London, e começa a reinventar a vida profissional e afetiva ¿ e a se abrir para antigas e novas emoções.
+
+Lançando-se nesse universo desconhecido, Russ embarca com London numa jornada ao mesmo tempo assustadora e gratificante, que testará suas habilidades e seu equilíbrio emocional além do que ele poderia ter imaginado. Em Dois a dois, Nicholas Sparks conta a história de um homem que precisa se redescobrir e buscar qualidades que nem desconfiava possuir para lutar pelo que é mais importante na vida: aqueles que amamos.
+
+Sobre o autor:
+
+Nicholas Sparks lançou seu primeiro romance aos 31 anos, ao qual se seguiram outros 19 livros. Suas obras foram traduzidas para 50 idiomas e já venderam mais de 100 milhões de exemplares no mundo todo. Onze de seus livros ganharam adaptações para o cinema. Dois a dois é um best-seller do The New York Times, tendo ficado em 1º lugar nas listas de mais vendidos do The Wall Street Journal e da Publishers Weekly. O autor mora na Carolina do Norte e tem cinco filhos.
+
+', 2, 'Imagens Meramente Ilustrativas.
+Todas as informações divulgadas são de responsabilidade do fabricante/fornecedor.', 28.99, 'https://images-submarino.b2w.io/produtos/01/00/item/131859/1/131859181_1GG.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Livro - Até O Fim', 4, 'O detetive Nap Dumas nunca mais foi o mesmo após o último ano do colégio, quando seu irmão Leo e a namorada, Diana, foram encontrados mortos nos trilhos da ferrovia. Além disso, Maura, o amor da vida de Nap, terminou com ele e desapareceu sem justificativa.Por quinze anos, o detetive procurou pela ex-namorada e buscou a verdadeira razão por trás da morte do irmão. Agora, parece que finalmente há uma pista.
+
+As digitais de Maura surgem no carro de um suposto assassino e Nap embarca em uma jornada por explicacões, que apenas levam a mais perguntas: sobre a mulher que amava, os amigos de infância que pensava conhecer, a base militar próxima a sua antiga casa. Em meio às investigações, Nap percebe que as mortes de Leo e Diana são ainda mais sombrias e sinistras do que ele ousava imaginar.
+
+¿Harlan Coben é mestre em prender a atencão do leitor e criar histórias surpreendentes. Ele vai seduzir você logo na primeira página só para chocá-lo na última.¿ - Dan Brown, autor de O Código Da Vinci
+
+¿Harlan Coben é um dos meus autores favoritos. Seus livros têm tudo que se pode esperar: suspense de roer as unhas, tramas vertiginosas, questões sociais relevantes e personagens perfeitos.¿ - Kristin Hannah, autora de O Rouxinol.', 2, 'Imagens meramente ilustrativas.
+Todas as informações divulgadas são de responsabilidade do Fabricante/Fornecedor.', 34.99, 'https://images-submarino.b2w.io/produtos/01/00/offers/01/00/item/134186/3/134186365SZ.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Livro - A Caça', 5, 'O ano letivo começou e Greer ­MacDonald está se esforçando ao máximo para se adaptar ao colégio interno onde ela entrou como bolsista. O problema é que a STAGS, além de ser a escola mais antiga e tradicional da Inglaterra, é repleta de alunos ricos e privilegiados ¿ tudo o que Greer não é. Para sua grande surpresa, um dia Greer recebe um cartão misterioso com apenas três palavras: "caça tiro pesca". Trata-se de um convite para passar o feriado na propriedade de Henry de Warlencourt, o garoto mais bonito e popular do colégio... e líder dos medievais, o grupo de alunos que dita as regras. Greer se junta ao clã de Henry e a outros colegas escolhidos para o evento, mas esse conto de fadas não vai terminar da maneira que ela imagina. À medida que os três esportes se tornam mais sombrios e estranhos, Greer se dá conta de que os predadores estão à espreita... e eles querem sangue. Que a caçada comece!
+
+"M. A. Bennett cria uma atmosfera arrepiante a cada página. É como um mistério de Arthur Conan Doyle ou Agatha Christie para adolescentes modernos." ¿ School Library Journal
+
+"Gossip Girl encontra Jogos vorazes." ¿ Bustle
+
+"Uma crítica contundente sobre o poder de sedução dos privilégios." ¿ The Guardian
+
+"Como Meninas malvadas, só que britânico e mortal. Um livro excelente, do início ao fim." ¿ Hypable', 2, 'Imagens Meramente Ilustrativas.', 34.99, 'https://images-submarino.b2w.io/produtos/01/00/offers/01/00/item/134161/8/134161838SZ.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Game The Legend of Zelda - The Wind Waker - Wii U', 6, 'Uma das maiores aventuras de todos os tempos retorna em alta definição The Legend of Zelda - The Wind Waker faz um glorioso retorno no console Wii U, com belíssimos gráficos em alta definição e recursos de jogo aprimorados. Nesse clássico eterno com um estilo artístico colorido e único, semelhante a um desenho animado, os jogadores guiam Link enquanto ele veleja pelo massivo Great Sea para encontrar sua irmã raptada. Os jogadores navegarão pelos mares em busca de ilhas perdidas, lutarão contra inimigos assustadores, enfrentarão chefes poderosos e buscarão a lendária Triforce.
+
+The Legend of Zelda - The Wind Waker HD apresenta visual em 1080p, para que os jogadores mergulhem no jogo como nunca antes. Com o controle Wii U GamePad, é fácil gerenciar seu inventário e ver mapas.Os jogadores poderão aproveitar a viagem pelo mar aberto com uma nova vela que dá 50% mais velocidade, levando-os ao próximo destino e à aventura cheia de ação ainda mais rapidamente. Assim que os jogadores receberem o item ¿Tingle Bottle¿, eles poderão se conectar a outras pessoas publicando comentários no Miiverse. Com esse novo recurso, comentários ou capturas de tela publicados no Miiverse chegarão à praia nos jogos de outras pessoas. Será como encontrar uma mensagem em uma garrafa. ', 1, 'Todas as informações são de responsabilidade do Fabricante/Fornecedor.
+
+Imagem Meramente Ilustrativa. ', 126, 'https://images-submarino.b2w.io/produtos/01/00/item/116708/0/116708019_1GG.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Game Pikmin 3 - Wii U', 9, 'Explore um mundo alienígena em um jogo como nenhum outro.Em Pikmin 3, os jogadores assumem o comando de três exploradores e uma legião de adoráveis Pikmin em uma luta pela sobrevivência. Usando as habilidades únicas dessas pequenas criaturas Pikmin, os jogadores solucionam quebra-cabeças, enfrentam perigosos predadores e recuperam comida, necessária para salvar o planeta natal dos exploradores. Descrição do produto: Conheça as fascinantes personalidades das criaturas vegetais Pikmin, que ganham vida em diversas formas e tamanhos.
+
+Cada tipo diferente de Pikmin, incluindo os novos Rock Pikmin e Winged Pikmin, oferece habilidades únicas. Rock Pikmin são criaturas resistentes que podem ser atiradas em objetos sólidos para quebrá-los.Winged Pikmin têm a capacidade de voar, e podem carregar objetos pelo ar. Divida e conquiste! Divida seus esquadrões de Pikmin entre os três exploradores para maximizar sua estratégia. Use o controle Wii U GamePad como câmera do jogo para observar o ambiente de safari alienígena com uma perspectiva afastada. Tire fotos, crie legendas e compartilhe-as no Miiverse. ', 1, 'Todas as informações são de responsabilidade do Fabricante/Fornecedor.
+
+Imagem Meramente Ilustrativa. ', 144.9, 'https://images-submarino.b2w.io/produtos/01/00/item/116707/8/116707876_1GG.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Game New Super Mario Bros. U - Wii U', 7, 'New Super Mario Bros. U é uma nova aventura com movimentação lateral incluindo Mario, Luigi, Toad¿ E o seu personagem Mii! Agora é a sua chance de entrar no Mushroom Kingdom e explorar novos mundos, novos power-ups e novas maneiras de jogar. ', 1, 'Todas as informações são de responsabilidade do Fabricante/Fornecedor.
+
+Imagem Meramente Ilustrativa.', 269.9, 'https://images-submarino.b2w.io/produtos/01/00/item/116707/9/116707930_1GG.jpg');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Game - Starfox Zero - Wii U', 10, 'Veja Novos Videos Enquanto Mata Ordas Frenéticas De Ataques Robôs.
+Defenda A Sua Instalação Do Forças Do Mal (Mas Fofinhos) Da Andross
+Crie Suas Próprias Ordas De Ataque Inimigas E Compartilhe Com Seus Amigos
+Suba De Nível Para Destravar Mais Missões E Câmeras Especiais', 1, '', 100, '');
+INSERT INTO public.products (name, id, description, category_id, notes, price, image) VALUES ('Game - Darksiders II - Wii U', 8, 'Edição da série que segue as aventuras de Death - Morte, um dos quatro Cavaleiros do Apocalipse, numa história paralela aos eventos do jogo original. ', 1, 'Imagens meramente ilustrativas
+Todas informações são de responsabilidade do fabricante/fornecedor
+', 170, 'https://images-submarino.b2w.io/produtos/01/00/item/120297/7/120297769_1GG.jpg');
